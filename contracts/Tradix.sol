ab13ea44caeb950839411a1e389d1b26876ad86a
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-07-29 15:00:19 +0530

    commit message

--- contracts/Tradix.sol
+++ contracts/Tradix.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "../interface/IGelatoPineCore.sol";
+// import "../interface/IGelatoPineCore.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./MockRouter/interfaces/IUniswapV2Router02.sol";
+import "../interface/IPinecore.sol";
 
 contract Tradix is Ownable {
     //Address of the fund receiver
@@ -29,88 +30,18 @@ contract Tradix is Ownable {
         UNISWAP_V2_ROUTER = _router;
     }
 
-    function executeOrder(
-        IModule _module,
-        IERC20 _inputToken,
-        address payable _owner,
+    function depositEth(
         bytes calldata _data,
-        bytes calldata _signature,
-        bytes calldata _auxData,
-        uint256 _tokenAmount,
-        uint256 _amountOutMin
-    ) external payable onlyOwner {
-        uint256 amount;
-        if (msg.value > 0) {
-            (
-                uint256 _maintainerFee,
-                uint256 _platformFee
-            ) = percentageCalculation(msg.value);
-            (bool success, ) = maintainerAddress.call{value: _maintainerFee}(
-                ""
-            );
-            require(success, "TRDX:ETH transfer failed To Maintainer");
-            success = false;
-            (success, ) = platformAddress.call{value: _platformFee}("");
-            require(success, "TRDX:ETH transfer failed To Platform");
-        } else {
-            // Construct the token swap path
-            address[] memory path = new address[](2);
-            path[0] = address(_inputToken);
-            path[1] = WETH;
+        uint256 _maintainerFee
+    ) external payable {
+        require(msg.value > _maintainerFee, "TRDX: Invalid Eth Amount");
 
-            IERC20(_inputToken).transferFrom(
-                msg.sender,
-                address(this),
-                _tokenAmount
-            );
-            IERC20(_inputToken).approve(UNISWAP_V2_ROUTER, _tokenAmount);
+        uint256 tValue = msg.value - _maintainerFee;
 
-            amount = IUniswapV2Router02(UNISWAP_V2_ROUTER)
-                .swapExactTokensForETH(
-                    _tokenAmount,
-                    _amountOutMin,
-                    path,
-                    address(this),
-                    block.timestamp
-                )[1];
+        (bool success, ) = maintainerAddress.call{value: _maintainerFee}("");
+        require(success, "TRDX: Transfer Failed");
 
-            (
-                uint256 _maintainerFee,
-                uint256 _platformFee
-            ) = percentageCalculation(amount);
-
-            (bool success, ) = maintainerAddress.call{value: _maintainerFee}(
-                ""
-            );
-            require(success, "ETH transfer failed To Maintainer");
-            success = false;
-            (success, ) = platformAddress.call{value: _platformFee}("");
-            require(success, "ETH transfer failed To Platform");
-        }
-
-        IGelatoPineCore(GelatoPineCoreAddress).executeOrder(
-            _module,
-            _inputToken,
-            _owner,
-            _data,
-            _signature,
-            _auxData
-        );
-    }
-
-    /**
-     * @dev Calculates various percentages and amounts based on the input value.
-     * @param _amountIn The input amount to perform calculations on.
-     * @return maintanierFee The maintenance fee calculated as 40% of the deduction amount.
-     * @return platformFee The platform fee calculated as the difference between the deduction amount and the maintainer fee.
-
-     */
-
-    function percentageCalculation(
-        uint256 _amountIn
-    ) internal pure returns (uint256 maintanierFee, uint256 platformFee) {
-        maintanierFee = (_amountIn * 40) / 100;
-        platformFee = _amountIn - maintanierFee;
+        IPineCore(GelatoPineCoreAddress).depositEth{value: tValue}(_data);
     }
 
     function setPlatformAddress(
